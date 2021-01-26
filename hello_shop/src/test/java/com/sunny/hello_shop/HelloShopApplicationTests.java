38d65f39e1876bb1ce150123268ba871a40acb93
seonyoung-jeon <seonyoung.jeon@nhnsoft.com>
2021-01-26 19:33:47 +0900

    init

---
+++ hello_shop/src/test/java/com/sunny/hello_shop/HelloShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.hello_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
